ff630aac4c161566385b12cecda678f8c069c718
권민석 <chris0540@naver.com>
2024-03-26 20:03:16 +0900

    [권민석]  - jsp 기본 설정  - 회원 가입 페이지 틀 잡기 #9

---
+++ src/main/java/com/rimin/mall/user/UserController.java
@@ -0,0 +1,18 @@
+package com.rimin.mall.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	
+	@GetMapping("/user/join")
+	public String logIn() {
+		return "user/join";
+	}
+	
+	
+	
+	
+}
